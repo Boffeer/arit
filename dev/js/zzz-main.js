e28baaad19dc207645525f1c6d5b8d149e60c950
boffeer <boffeechane@gmail.com>
2021-02-27 19:40:31 +0300

    complete general layout

--- dev/js/zzz-main.js
+++ dev/js/zzz-main.js
@@ -12,3 +12,32 @@ const teachersSlider = new Swiper('.teachers-slider-wrap',{
 	// centerInsufficientSlides: true,
 	centeredSliderBounds: true,
 })
+const newsSlider = new Swiper('.news-slider-wrapper',{
+	slidesPerView: 3,
+	spaceBetween: 38,
+	navigation: {
+		nextEl: '.news-slider-next',
+		prevEl: '.news-slider-prev',
+	},
+	scrollbar: {
+		el: '.news-slider-scrollbar',
+		hide: false,
+	},
+	centerInsufficientSlides: true,
+	centeredSliderBounds: true,
+})
+
+const partnersSlider = new Swiper('.partners-slider-wrap',{
+	slidesPerView: 5,
+	spaceBetween: 20,
+	navigation: {
+		nextEl: '.partners-slider-next',
+		prevEl: '.partners-slider-prev',
+	},
+	scrollbar: {
+		el: '.partners-slider-scrollbar',
+		hide: false,
+	},
+	centerInsufficientSlides: true,
+	centeredSliderBounds: true,
+})
