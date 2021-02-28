0cd2a91cd09ab71a832b82ae07517005f11ed38d
boffeer <boffeechane@gmail.com>
2021-02-28 12:58:46 +0300

    desktop

--- dev/js/zzz-main.js
+++ dev/js/zzz-main.js
@@ -1,6 +1,29 @@
+const skillupTabHandler = document.querySelector('.learning-tab__control--skillup');
+const skillupTab = document.querySelector('.learning-tab--skillup');
+const retrainTabHandler = document.querySelector('.learning-tab__control--retrain');
+const retrainTab = document.querySelector('.learning-tab--retrain');
+
+skillupTabHandler.addEventListener('click', function(){
+	this.classList.add('learning-tab__control--active')
+	skillupTab.classList.add('learning-tab--active');
+	retrainTabHandler.classList.remove('learning-tab__control--active')
+	retrainTab.classList.remove('learning-tab--active')
+})
+retrainTabHandler.addEventListener('click', function(){
+	this.classList.add('learning-tab__control--active');
+	retrainTab.classList.add('learning-tab--active');
+	skillupTabHandler.classList.remove('learning-tab__control--active');
+	skillupTab.classList.remove('learning-tab--active');
+})
+
+
+// sliders
 const teachersSlider = new Swiper('.teachers-slider-wrap',{
+	// slidesPerView: 1,
 	slidesPerView: 3,
-	spaceBetween: 60,
+	slidesPerGroup: 3,
+	slidesPerColumn: 1,
+	spaceBetween: 50,
 	navigation: {
 		nextEl: '.teachers-slider-next',
 		prevEl: '.teachers-slider-prev',
@@ -11,10 +34,19 @@ const teachersSlider = new Swiper('.teachers-slider-wrap',{
 	},
 	// centerInsufficientSlides: true,
 	centeredSliderBounds: true,
+	breakpoints: {
+		// when window width is >= 320px
+		1200: {
+			slidesPerView: 3,
+		},
+		
+	}
 })
 const newsSlider = new Swiper('.news-slider-wrapper',{
 	slidesPerView: 3,
-	spaceBetween: 38,
+	// slidesPerGroup: 3,
+	slidesPerColumn: 1,
+	spaceBetween: 20,
 	navigation: {
 		nextEl: '.news-slider-next',
 		prevEl: '.news-slider-prev',
@@ -41,3 +73,7 @@ const partnersSlider = new Swiper('.partners-slider-wrap',{
 	centerInsufficientSlides: true,
 	centeredSliderBounds: true,
 })
+
+
+
+
